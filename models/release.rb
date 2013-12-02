08f50ae72d0b808cc95d5df5919ace6d7c00e395
Samuel Prashker <sorry@noemail.ca>
2013-12-02 17:38:19 -0500

    logical deletion field for release
    Might need to do the same for users and comments, but we'll take it one
    step at a time (base class?)

--- models/release.rb
+++ models/release.rb
@@ -5,6 +5,7 @@ class Release
     field :filesize, type: Integer
     field :numfiles, type: Integer
     field :added, type: DateTime
+	field :logicallyDeleted, type: Boolean, default: false
     
     has_and_belongs_to_many :user
     has_many :comments  
